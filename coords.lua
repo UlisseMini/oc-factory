b390436318eaf2a008ec03e518867a0482b5253c
Ulisse Mini <ulisse.mini@gmail.com>
2020-01-16 14:26:27 -0500

    Use two spaces not tabs, Like god intended

--- coords.lua
+++ coords.lua
@@ -27,13 +27,13 @@ local delta = {
 }
 
 function t.turnRight()
-	robot.turnRight()
-	c.ori = (c.ori + 1) % 4
+  robot.turnRight()
+  c.ori = (c.ori + 1) % 4
 end
 
 function t.turnLeft()
-	robot.turnLeft()
-	c.ori = (c.ori - 1) % 4
+  robot.turnLeft()
+  c.ori = (c.ori - 1) % 4
 end
 
 --- Create a new move function.
@@ -58,10 +58,10 @@ t.down    = move(robot.down, function() c.y = c.y - 1 end)
 --- Needed for converting the orientation back and forth to strings.
 -- if a key does not exist an error is thrown.
 local oris = {
-	["north"] = 0,
-	["east"]  = 1,
-	["south"] = 2,
-	["west"]  = 3
+  ["north"] = 0,
+  ["east"]  = 1,
+  ["south"] = 2,
+  ["west"]  = 3
 }
 
 --- Look a direction,
@@ -69,13 +69,13 @@ local oris = {
 -- if it is a string then it will be converted to a number based
 -- on the oris table.
 function t.look(direction)
-	if type(direction) == "string" then
+  if type(direction) == "string" then
     if oris[direction] == nil then
       error(direction .. ' is not in the orientations table')
     end
 
     direction = oris[direction]
-	end
+  end
 
   -- Now we turn to the correct orientation
   if c.ori == (direction + 1) % 4 then
@@ -125,7 +125,7 @@ oriT = %q (want number or string)
   end
 
   while yT < c.y do
-		moveWith(robot.swingDown, t.down)
+    moveWith(robot.swingDown, t.down)
   end
 
   while yT > c.y do
