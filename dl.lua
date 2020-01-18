c1e1bc802514ab210dcfaad45e73e35bf0537614
Ulisse Mini <ulisse.mini@gmail.com>
2020-01-18 15:39:14 -0500

    Add test module and dl script

---
+++ dl.lua
@@ -0,0 +1,7 @@
+local get = function(...)
+  for _, file in ipairs({...}) do
+    shell.run('wget -f https://raw.githubusercontent.com/UlisseMini/oc-factory/master/' .. file)
+  end
+end
+
+get({'coords.lua', 'factory.lua', 'modules/test.lua', 'quarry.lua'})
---
+++ modules/test.lua
@@ -0,0 +1,9 @@
+local main = function(t)
+  t.swing()
+end
+
+return main, {
+  level = 141,
+  interval = 120,
+  ori = 'west',
+}
