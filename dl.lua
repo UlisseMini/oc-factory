7365d2690734dd92f2db8e1262eb45f9e6189b3a
Ulisse Mini <ulisse.mini@gmail.com>
2020-01-18 15:42:44 -0500

    go die shell api

--- dl.lua
+++ dl.lua
@@ -2,7 +2,7 @@ local shell = require('shell')
 
 local get = function(...)
   for _, file in ipairs({...}) do
-    shell.run('wget -f https://raw.githubusercontent.com/UlisseMini/oc-factory/master/' .. file)
+    shell.execute('wget -f https://raw.githubusercontent.com/UlisseMini/oc-factory/master/' .. file)
   end
 end
 
