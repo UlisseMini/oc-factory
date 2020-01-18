1330be924e6baa1aebed9d4f11474a70f3ccaa48
Ulisse Mini <ulisse.mini@gmail.com>
2020-01-18 15:40:33 -0500

    forgot to require shell

--- dl.lua
+++ dl.lua
@@ -1,3 +1,5 @@
+local shell = require('shell')
+
 local get = function(...)
   for _, file in ipairs({...}) do
     shell.run('wget -f https://raw.githubusercontent.com/UlisseMini/oc-factory/master/' .. file)
