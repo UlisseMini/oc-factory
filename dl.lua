af635fb13041980be44f26f8cec1dd5fd2dfb3ef
Ulisse Mini <ulisse.mini@gmail.com>
2020-01-22 12:12:46 -0500

    Remove old files

--- dl.lua
+++
@@ -1,9 +0,0 @@
-local shell = require('shell')
-
-local get = function(...)
-  for _, file in ipairs({...}) do
-    shell.execute('wget -f https://raw.githubusercontent.com/UlisseMini/oc-factory/master/' .. file)
-  end
-end
-
-get('coords.lua', 'factory.lua', 'modules/test.lua', 'quarry.lua')
--- modules/test.lua
+++
@@ -1,9 +0,0 @@
-local main = function(t)
-  t.swing()
-end
-
-return main, {
-  level = 141,
-  interval = 120,
-  ori = 'west',
-}
